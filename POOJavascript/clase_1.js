ee1c8beb2b722d3455046c4da7d18c597b8ac5cc
caortizpacheco <ca.ortiz.pacheco@gmail.com>
2020-07-13 17:09:23 -0500

    first commit

---
+++ POOJavascript/clase_1.js
@@ -0,0 +1,12 @@
+function Persona (nombre,apellido) {
+    this.nombre = nombre 
+    this.apellido = apellido
+}
+
+console.log("TEST")
+
+
+
+let carlos = new Persona("carlos","ortiz")
+
+console.log(carlos)
